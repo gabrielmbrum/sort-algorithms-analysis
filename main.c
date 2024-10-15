28663735d6fc082bad19757d3d469808d20faf4c
Brum <gabrielbrum2805@gmail.com>
2024-10-14 22:02:10 -0300

    testing

--- main.c
+++ main.c
@@ -57,6 +57,26 @@ int *quicksort(int *a, int n) {
     return a;
 }
 
+/*------------------------ selection sort ------------------------*/
+int *selectionSort(int *vet, int size) {
+    for (int i = size-1; i >=0; i--) {
+        int maior = i;
+        for (int j = 0; j < i; j++) {
+            if (vet[j] > vet[maior]) {
+                maior = j; // Atualiza o maior se necessário
+            }
+        }
+
+        // Troca os elementos
+        if (maior != i) { // Verifica se é necessário trocar
+            int aux = vet[i];
+            vet[i] = vet[maior];
+            vet[maior] = aux;
+        }
+    }
+    return vet;
+}
+
 /*--------------------------- bubble sort------------------------*/
 int* bubblesort (int *a, int n) {
 	int i, j;
